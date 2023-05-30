cb9842eb315bb23d27c246a1ec1c36e192cca30b
arsalanahmad4 <arsalanahmad0407@gmail.com>
2023-05-30 22:57:57 +0530

    font&resources

--- app/src/main/java/com/example/aisle/EnterNumberActivity.kt
+++ app/src/main/java/com/example/aisle/EnterNumberActivity.kt
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class EnterNumberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_enter_number)
     }
 }
